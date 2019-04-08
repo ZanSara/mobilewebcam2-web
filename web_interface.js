0f718fc4c9641976881342000e47afbff20d72b5
ZanSara <sarazanzo94@gmail.com>
2019-04-08 16:18:33 +0200

    Stub of the web interface. Now reads the JSON, rendering is still full of bugs

---
+++ web_interface.js
@@ -0,0 +1,73 @@
+
+function populate_web_interface(){
+
+    settings = JSON.parse(mwc2config);
+    console.log(settings);
+    
+    html_card_head = `
+    <div class="card">
+        <div class="card-header" id="heading_{{name}}">
+          <a class="mb-0" data-toggle="collapse" data-target="#collapse_{{name}}" aria-expanded="false" aria-controls="collapse_{{name}}">
+                <h4 style="margin-bottom:0;">{{fullName}}</h4>
+                <small style="font-style: italic; color:#555555;">{{description}}</small>
+          </a>
+        </div>
+        <div id="collapse_{{name}}" class="collapse" aria-labelledby="heading_{{name}}" data-parent="#accordion">
+          <div class="card-body">
+          
+          {{content}}
+          
+          </div>
+        </div>
+      </div>`
+      
+    html_regular_field = `
+                <div class="form-group row">
+                    <label for="field_{{name}}" class="col-sm-2 col-form-label">{{fullName}}</label>
+                    <div class="col-sm-10">
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="field_{{name}}" aria-describedby="field_{{name}}_desc" placeholder="{{defaultValue}}" value="{{value}}">
+                            <div class="input-group-append">
+                              <div class="input-group-text">{{unit}}</div>
+                            </div>
+                        </div>
+                        <small id="field_{{name}}_desc" class="form-text text-muted">{{description}}</small>
+                    </div>
+                  </div>`
+    
+    blocks_rendered = render_template(settings, html_card_head, html_regular_field);
+    document.getElementById("accordion").innerHTML = blocks_rendered;
+
+}
+
+
+
+function render_template(data, html_outer_template, html_inner_template){
+    var final_html = "";
+    
+    for (var key in data) {
+      block_html = html_outer_template;
+      block_html = block_html.split("{{name}}").join(key);
+      
+      for(var subkey in data[key]){
+        block_html = block_html.split("{{"+subkey+"}}").join(data[key][subkey]);
+      }
+      
+      content_html = "";
+      // Now process the inner fields
+      for(var field in data[key]["value"]){
+          field_html = html_inner_template;
+          field_html = field_html.split("{{name}}").join(field);
+          
+          for(var subfield in data[key]["value"][field]){
+            field_html = field_html.split("{{"+subfield+"}}").join(data[key]["value"][field][subfield]);
+          }
+          content_html += field_html;
+      }
+      block_html = block_html.split("{{content}}").join(content_html);
+      
+      final_html += block_html;
+    }
+                   
+    return final_html;
+}
